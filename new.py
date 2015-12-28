e13858c91703e728ea3b00e7e3a1827a7872d472
Danny Loffredo <loffredo@gmail.com>
2015-12-27 21:06:44 -0700

    more goodness

--- new.py
+++ new.py
@@ -6,7 +6,8 @@ import cPickle as pickle
 from util import list_dirs, list_files_recursive
 
 from sklearn.feature_extraction.text import HashingVectorizer
-# from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import pairwise_distances
+from scipy.sparse import coo_matrix
 
 class Source(object):
     def __init__(self, path):
@@ -24,14 +25,14 @@ class Source(object):
         x = HashingVectorizer(input="filename", encoding="latin-1", decode_error="replace",
                               binary=False, ngram_range=(4,4))\
             .fit_transform(self.files)
-        logging.debug(str(x))
+        logging.debug(repr(x))
         self.features = x
 
     def load_features(self, filename):
         logging.debug("loading features for %s from %s" % (self.name, filename))
         with open(filename) as f:
             x = pickle.load(f)
-        logging.debug(str(x))
+        logging.debug(repr(x))
         self.features = x
 
     def save_features(self, filename):
@@ -39,6 +40,17 @@ class Source(object):
         logging.debug("saving features for %s to %s" % (self.name, filename))
         with open(filename, "w") as f:
             pickle.dump(self.features, f, -1)
+
+    def build_distance_matrix(self, other):
+        assert self.features is not None
+        assert other.features is not None
+        logging.debug("calculating distance %s to %s" % (self.name, other.name))
+        z = pairwise_distances(self.features, other.features, metric='cosine', n_jobs=-1)
+        return z
+
+    def build_similarity_matrix(self, other, threshold):
+        z = self.build_distance_matrix(other)
+        return coo_matrix(z < threshold)
 
 class Smallworld(object):
     def __init__(self, path, cache_path=None):
@@ -69,7 +81,7 @@ class Smallworld(object):
     def resume(self):
         # load or calculate features for each source
         for src in self.sources:
-            path = os.path.join(self.cache_path, src.name)
+            path = os.path.join(self.cache_path, "%s.features" % src.name)
             if os.path.exists(path):
                 src.load_features(path)
             else:
@@ -82,6 +94,8 @@ class Smallworld(object):
     def save(self):
         # save similarity results
         pass
+
+
 
 
 if __name__ == "__main__":
@@ -101,4 +115,10 @@ if __name__ == "__main__":
 
     s = Smallworld(args.path)
     s.resume()
+    for a in s.sources:
+        for b in s.sources:
+            if a == b: continue
+            a.build_distance_matrix(b)
+            # d[a.name, b.name] = a.build_similarity_matrix(b, threshold)
+
     s.save()
