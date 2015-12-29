95e52c03fd38fa9b20baa48a6064a8773a8309f8
Danny Loffredo <loffredo@gmail.com>
2015-12-29 12:12:12 -0700

    file cleanup

--- main.py
+++ main.py
@@ -1,125 +1,150 @@
 import os
+import errno
+import logging
+import cPickle as pickle
+import itertools
+
+from util import list_dirs, list_files_recursive
+
 from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.metrics.pairwise import pairwise_distances
-import cPickle as pickle
-import scandir # pip install scandir
-from collections import defaultdict
-import numpy as np
+from scipy.sparse import coo_matrix
 
-def build_feature_matrix(filenames, cache_file=None, force=False):
-    if cache_file and os.path.exists(cache_file) and not force:
-        print "using feature matrix cache %s" % cache_file
-        with open(cache_file) as f:
-            x = pickle.load(f)
-    else:
-        print "calculating feature vector"
-        x = HashingVectorizer(input="filename", encoding="latin-1", decode_error="replace",
-                               binary=False, ngram_range=(4,4))\
-            .fit_transform(filenames)
-        if cache_file:
-            print "writing cache file %s" % cache_file
-            with open(cache_file, "w") as f:
-                pickle.dump(x, f, -1)
-    return x
 
-def build_distance_matrix(x, y, cache_file=None, force=False):
-    if cache_file and os.path.exists(cache_file) and not force:
-        print "using distance matrix cache %s" % cache_file
-        with open(cache_file) as f:
-            z = pickle.load(f)
-    else:
-        print "calculating connectivity matrix"
-        z = pairwise_distances(x,y, metric='cosine', n_jobs=-1)
-        if cache_file:
-            print "writing cache file %s" % cache_file
-            with open(cache_file, "w") as f:
-                pickle.dump(z, f, -1)
-    return z
-
-def calc_similarity(x, y, threshold, force=False):
-    z = build_distance_matrix(x, y, force=force)
-    return z
-
-def subfiles(path):
-    all_files = []
-    for root, dirs, files in os.walk(path):
-        files = [os.path.join(root, f) for f in files if not f.startswith('.')]
-        # don't visit hidden files
-        dirs[:] = [d for d in dirs if not d.startswith('.')]
-        all_files.extend(files)
-    return all_files
-
-def subdirs(path):
-    return [ entry.path for entry in scandir.scandir(path) if entry.is_dir() ]
-
-def iterarray(arr):
-    it = np.nditer(arr, flags=['multi_index'])
-    while not it.finished:
-        i,j = it.multi_index
-        v = it[0]
-        yield i,j,v
-        it.iternext()
-
-class SrcPackage(object):
-    def __init__(self, root_dir):
-        self.name = os.path.basename(root_dir)
-        self.path = root_dir
-        self.files = list(subfiles(root_dir))
-        self.similar_count = defaultdict(int)
+class Source(object):
+    def __init__(self, path):
+        self.name = os.path.basename(path)
+        self.path = os.path.abspath(path)
+        self.files = list_files_recursive(path)
+        self.features = None
+        logging.info(str(self))
 
     def __str__(self):
-        return "%s (%d files)" % (self.name, len(self.files))
+        return "Source(%s, %d files)" % (self.name, len(self.files))
 
-    def build_feature_matrix(self, force=False):
-        self.features = build_feature_matrix(self.files, force=force)
+    def build_features(self):
+        logging.debug("calculating features for %s" % self.name)
+        x = HashingVectorizer(input="filename", encoding="latin-1", decode_error="replace",
+                              binary=False, ngram_range=(4, 4))\
+            .fit_transform(self.files)
+        logging.debug(repr(x))
+        self.features = x
+
+    def load_features(self, filename):
+        logging.debug("loading features for %s from %s" % (self.name, filename))
+        with open(filename) as f:
+            x = pickle.load(f)
+        logging.debug(repr(x))
+        self.features = x
+
+    def save_features(self, filename):
+        assert self.features is not None
+        logging.debug("saving features for %s to %s" % (self.name, filename))
+        with open(filename, "w") as f:
+            pickle.dump(self.features, f, -1)
+
+    def build_distance_matrix(self, other):
+        assert self.features is not None
+        assert other.features is not None
+        logging.debug("calculating distance %s to %s" % (self.name, other.name))
+        z = pairwise_distances(self.features, other.features, metric='cosine', n_jobs=-1)
+        return z
+
+    def build_similarity_matrix(self, other, threshold):
+        z = self.build_distance_matrix(other)
+        return coo_matrix(z < threshold)
+
+
+class Smallworld(object):
+    THRESHOLD = 0.5
+
+    def __init__(self, path, cache_path=None):
+        self.path = os.path.abspath(path)
+        self.dirs = list_dirs(path)
+        self.sources = [Source(p) for p in self.dirs]
+        self.similarity_matrices = None
+
+        if cache_path is not None:
+            self.cache_path = cache_path
+        else:
+            self.cache_path = os.path.join(path, '.smallworld')
+        logging.debug('using cache_path %s' % self.cache_path)
+
+        # mkdir cache_path
+        try:
+            os.mkdir(self.cache_path)
+        except OSError as e:
+            if e.errno == errno.EEXIST:
+                pass
+            else:
+                raise
+
+        logging.info(str(self))
+
+    def __str__(self):
+        return "Smallworld(%s, %d sources)" % (self.path, len(self.sources))
+
+    @staticmethod
+    def make_key(a,b):
+        r,s = sorted((a.name,b.name))
+        key = "%s,%s" % (r, s)
+        return key
+
+    def resume(self):
+        self.get_features()
+        self.get_similarity()
+
+    def get_features(self):
+        # load or calculate features for each source
+        for src in self.sources:
+            path = os.path.join(self.cache_path, "%s.features" % src.name)
+            if os.path.exists(path):
+                src.load_features(path)
+            else:
+                src.build_features()
+                src.save_features(path)
+
+    def get_similarity(self):
+        self.similarity_matrices = {}
+        # TODO fix normalized / threshold problem
+        # load or calculate similarity results
+        for i,a in enumerate(self.sources):
+            for b in self.sources[i + 1:]:
+                key = self.make_key(a,b)
+                path = os.path.join(self.cache_path, "%s.similarity" % key)
+                if os.path.exists(path):
+                    logging.debug("loading similarity matrix from %s" % path)
+                    with open(path) as f:
+                        m = pickle.load(f)
+                else:
+                    m = a.build_similarity_matrix(b, threshold=self.THRESHOLD)
+                    logging.debug("saving similarity matrix to %s" % path)
+                    with open(path, 'w') as f:
+                        pickle.dump(m, f, -1)
+                self.similarity_matrices[(a,b)] = m
+
+    def print_matches(self):
+        for (a,b),m in self.similarity_matrices.iteritems():
+            print a.name, b.name
+            for i, j, v in itertools.izip(m.row, m.col, m.data):
+                print "  ", a.files[i], b.files[j]
+
 
 if __name__ == "__main__":
     import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("src_dir")
-    parser.add_argument("--force", action="store_true", default=False)
-    parser.add_argument("--limit", type=int, default=0)
-    parser.add_argument("--threshold", type=float, default=0.8)
+    parser.add_argument('path')
+    parser.add_argument('-v', '--verbose', action='count')
     args = parser.parse_args()
 
-    packages = [ SrcPackage(path) for path in subdirs(args.src_dir) ]
-    for pkg in packages:
-        print pkg
-        pkg.build_feature_matrix(force=args.force)
+    if args.verbose <= 0:
+        loglevel = logging.WARN
+    elif args.verbose == 1:
+        loglevel = logging.INFO
+    else:
+        loglevel = logging.DEBUG
+    logging.basicConfig(level=loglevel, format="%(message)s")
 
-    # file_owners = {}
-    # for package in packages:
-    #     for file in package.files:
-    #         file_owners[file] = package
-    #
-
-    for a in packages:
-        for b in packages:
-            if a == b: continue
-            similar = calc_similarity(a.features, b.features, threshold=0.2)
-            import pdb; pdb.set_trace()
-
-
-    # TODO pkg_connectivity(a,b) = (num similar files over thresold) / min(num a files, num b files)
-
-    for i,j,v in iterarray(sim):
-        if abs(v) > args.threshold: continue
-        if i == j: continue
-
-        print all_files[i], all_files[j]
-        print i, j, v
-        pkg_a = file_owners[ all_files[i] ]
-        pkg_b = file_owners[ all_files[j] ]
-        pkg_a.similar_count[pkg_b] += 1
-
-    for pkg_a in packages:
-        print pkg_a.name
-        for pkg_b, count in pkg_a.similar_count.iteritems():
-            print "  %-20s : %d" % (pkg_b.name, count)
-
-
-
-
-
-
+    s = Smallworld(args.path)
+    s.resume()
+    s.print_matches()
